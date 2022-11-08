0f533927546f9b8da012e9ed90094f2873f4a463
Amas9nine <amasdina2@gmail.com>
2022-11-08 16:23:26 +0600

    mmmm

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const url = "https://api.opensea.io/api/v1/assets?format=json";
+  const [todo, setTodo] = useState(null);
+
+  console.log("before fetch begin -> null of useState(null)")
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(json => setTodo(json))
+  }, [])
+  console.log(todo)
+
+  return (
+    <div className="App">
+      <h1>H! H! H!</h1>
+      {todo && <h2>{todo.assets[0].id}</h2>}
+      {todo && <img alt="dada" src={todo.assets[0].image_url} />}
+    </div>
+  );
+}
+
+export default App;
